dae38dd47b3446a8e6ccef8e90fc07ab7dbb0af6
Daniel <danielcederberg1@gmail.com>
2025-08-28 04:11:02 -0700

    merge fix

--- cvxpy/reductions/expr2smooth/canonicalizers/log_canon.py
+++ cvxpy/reductions/expr2smooth/canonicalizers/log_canon.py
@@ -16,6 +16,7 @@ limitations under the License.
 
 from cvxpy.expressions.variable import Variable
 from cvxpy.expressions.constants import Constant
+import numpy as np
 
 def collect_constant_and_variable(expr, constants, variable):
     if isinstance(expr, Constant):
