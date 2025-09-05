2fc0d2029aa2855a754fc5db7373b9452460d23d
William Zijie Zhang <william@gridmatic.com>
2025-09-05 11:20:00 +0200

    add value propagation

--- cvxpy/reductions/expr2smooth/canonicalizers/exp_canon.py
+++ cvxpy/reductions/expr2smooth/canonicalizers/exp_canon.py
@@ -13,9 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-import numpy as np
-
 from cvxpy.expressions.variable import Variable
 
 
@@ -24,4 +21,6 @@ def exp_canon(expr, args):
         return expr, []
     else:
         t = Variable(args[0].shape)
+        if args[0].value is not None:
+            t.value = args[0].value
         return expr.copy([t]), [t==args[0]]
