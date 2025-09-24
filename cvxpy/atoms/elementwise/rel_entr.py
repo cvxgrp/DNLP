fix most of the hessian tests, but others are failing

cvxpy/atoms/elementwise/rel_entr.py
@@ -123,15 +123,15 @@ class rel_entr(Elementwise):
         dxdy_vals = - vec / y.value
 
         if x.size == 1:
-            return {(x, x): sp.csr_array(np.sum(dx2_vals)),
+            return {(x, x): np.array(np.sum(dx2_vals)),
                     (y, y): sp.diags_array(dy2_vals),
-                    (x, y): sp.csr_array(dxdy_vals),
-                    (y, x): sp.csr_array(dxdy_vals)}
+                    (x, y): np.array(dxdy_vals),
+                    (y, x): np.array(dxdy_vals)}
         elif y.size == 1:
             return {(x, x): sp.diags_array(dx2_vals), 
-                    (y, y): sp.csr_array(np.sum(dy2_vals)),
-                    (x, y): sp.csr_array(dxdy_vals),
-                    (y, x): sp.csr_array(dxdy_vals)}
+                    (y, y): np.array(np.sum(dy2_vals)),
+                    (x, y): np.array(dxdy_vals),
+                    (y, x): np.array(dxdy_vals)}
         else:
             return {(x, x): sp.diags_array(dx2_vals), 
                     (y, y): sp.diags_array(dy2_vals),

cvxpy/tests/NLP_tests/hess_tests/test_hess_idx.py
@@ -14,7 +14,7 @@ class TestHessAdd():
         log2 = cp.log(x)[2]
         result_dict = log2.hess_vec(vec)
         result_correct = 4 * (-np.diag(np.array([0, 0, 1/9])))
-        assert(np.allclose(result_dict[(x, x)], result_correct))
+        assert(np.allclose(result_dict[(x, x)].toarray(), result_correct))
 
     def test_slice_two_idx(self):
         n = 3 
@@ -25,8 +25,8 @@ class TestHessAdd():
         log12 = cp.log(x)[idxs]
         result_dict = log12.hess_vec(vec)
         result_correct = 2 * (-np.diag(np.array([0, 1/4, 0]))) + \
-                         4 * (-np.diag(np.array([0, 0, 1/9]))) 
-        assert(np.allclose(result_dict[(x, x)], result_correct))
+                         4 * (-np.diag(np.array([0, 0, 1/9])))
+        assert(np.allclose(result_dict[(x, x)].toarray(), result_correct))
 
     
     def test_slice_two_other_idx(self):
@@ -39,4 +39,4 @@ class TestHessAdd():
         result_dict = log12.hess_vec(vec)
         result_correct = 2 * (-np.diag(np.array([1 / 2.25, 0, 0]))) + \
                              4 * (-np.diag(np.array([0, 0, 1/9])))
-        assert(np.allclose(result_dict[(x, x)], result_correct))
+        assert(np.allclose(result_dict[(x, x)].toarray(), result_correct))

cvxpy/tests/NLP_tests/hess_tests/test_hess_multiply.py
@@ -46,8 +46,8 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = np.diag(np.array([5.0, 4.0, 3.0]))
-        assert(np.allclose(result_dict[(x, y)], correct_matrix))
-        assert(np.allclose(result_dict[(y, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, y)].toarray(), correct_matrix))
+        assert(np.allclose(result_dict[(y, x)].toarray(), correct_matrix))
         assert(len(result_dict) == 2)
 
     # x * y with x scalar variable, y scalar variable
@@ -60,8 +60,8 @@ class TestHessVecMultiply():
         vec = np.array([5.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = np.array([[5.0]])
-        assert(np.allclose(result_dict[(x, y)], correct_matrix))
-        assert(np.allclose(result_dict[(y, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, y)].toarray(), correct_matrix))
+        assert(np.allclose(result_dict[(y, x)].toarray(), correct_matrix))
         assert(len(result_dict) == 2)
 
     # x * y with x vector variable, y scalar variable,
@@ -74,8 +74,8 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = np.array([5.0, 4.0, 3.0])
-        assert(np.allclose(result_dict[(x, y)], correct_matrix))
-        assert(np.allclose(result_dict[(y, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, y)].toarray(), correct_matrix))
+        assert(np.allclose(result_dict[(y, x)].toarray(), correct_matrix))
         assert(len(result_dict) == 2)
 
     # x * y with x scalar variable, y vector variable,
@@ -88,8 +88,8 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = np.array([5.0, 4.0, 3.0])
-        assert(np.allclose(result_dict[(x, y)], correct_matrix))
-        assert(np.allclose(result_dict[(y, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, y)].toarray(), correct_matrix))
+        assert(np.allclose(result_dict[(y, x)].toarray(), correct_matrix))
         assert(len(result_dict) == 2)
 
     # scalar constant * phi(x) 
@@ -101,7 +101,7 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = 3 * log.hess_vec(vec)[(x, x)]
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix.toarray()))
         assert(len(result_dict) == 1)
 
     # phi(x) * scalar constant
@@ -113,7 +113,7 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = 3 * log.hess_vec(vec)[(x, x)]
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix.toarray()))
         assert(len(result_dict) == 1)
 
     # vector constant * phi(x) 
@@ -126,7 +126,7 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = log.hess_vec(vec * constant_vector)[(x, x)]
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix.toarray()))
         assert(len(result_dict) == 1)
 
     # phi(x) * vector constant
@@ -139,6 +139,5 @@ class TestHessVecMultiply():
         vec = np.array([5.0, 4.0, 3.0])
         result_dict = mult.hess_vec(vec)
         correct_matrix = log.hess_vec(vec * constant_vector)[(x, x)]
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix.toarray()))
         assert(len(result_dict) == 1)
-

cvxpy/tests/NLP_tests/hess_tests/test_hess_rel_entr.py
@@ -51,8 +51,8 @@ class TestHessVecRelativeEntropy():
         result_dict = rel_entr.hess_vec(vec)
 
         H_computed = np.block([
-            [result_dict[(x, x)], result_dict[(x, y)]],
-            [result_dict[(y, x)], result_dict[(y, y)]]
+            [result_dict[(x, x)].toarray(), result_dict[(x, y)].toarray()],
+            [result_dict[(y, x)].toarray(), result_dict[(y, y)].toarray()]
         ])
 
         # compute Hessian manually
@@ -148,7 +148,3 @@ class TestHessVecRelativeEntropy():
         H[1:, 1:] = np.diag(vec * x / (y**2))
 
         assert(np.allclose(H_computed, H))
-
-
-
-

cvxpy/tests/NLP_tests/hess_tests/test_hess_sum.py
@@ -14,7 +14,7 @@ class TestHessSum():
         sum = cp.sum(cp.log(x))
         result_dict = sum.hess_vec(dummy_vec)
         result_correct = -3.5 * np.diag(1 / x.value ** 2)
-        assert(np.allclose(result_dict[(x, x)], result_correct))
+        assert(np.allclose(result_dict[(x, x)].toarray(), result_correct))
 
     def test_sum_two(self):
         n = 3 
@@ -24,7 +24,7 @@ class TestHessSum():
         sum = cp.sum(4 * cp.log(x))
         result_dict = sum.hess_vec(dummy_vec)
         result_correct = -4 * np.diag(1 / x.value ** 2)
-        assert(np.allclose(result_dict[(x, x)], result_correct))
+        assert(np.allclose(result_dict[(x, x)].toarray(), result_correct))
     
     def test_sum_three(self):
         n = 3 
@@ -34,4 +34,4 @@ class TestHessSum():
         sum = cp.sum(cp.multiply(np.array([1, 2, 3]), cp.log(x)))
         result_dict = sum.hess_vec(dummy_vec)
         result_correct = 4 * np.diag(-1 * np.array([1 / (1.0**2), 2 / (2.0**2), 3 / (3.0**2)]))
-        assert(np.allclose(result_dict[(x, x)], result_correct))
+        assert(np.allclose(result_dict[(x, x)].toarray(), result_correct))

cvxpy/tests/NLP_tests/hess_tests/test_hess_univariate_elementwise_atoms.py
@@ -13,7 +13,7 @@ class TestHessVecElementwiseUnivariate():
         log = cp.log(x)
         result_dict = log.hess_vec(vec)
         correct_matrix = np.diag([ -5.0/(1.0**2), -4.0/(2.0**2), -3.0/(3.0**2)])
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix))
 
     def test_constant_log(self):
         x = np.array([1.0, 2.0, 3.0])
@@ -30,7 +30,7 @@ class TestHessVecElementwiseUnivariate():
         exp = cp.exp(x)
         result_dict = exp.hess_vec(vec)
         correct_matrix = np.diag([ 5.0*np.exp(1.0), 4.0*np.exp(2.0), 3.0*np.exp(3.0)])
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix))
 
     def test_constant_exp(self):
         x = np.array([1.0, 2.0, 3.0])
@@ -47,7 +47,7 @@ class TestHessVecElementwiseUnivariate():
         entr = cp.entr(x)
         result_dict = entr.hess_vec(vec)
         correct_matrix = np.diag([ -5.0/(1.0), -4.0/(2.0), -3.0/(3.0)])
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix))
 
     def test_constant_entr(self):
         x = np.array([1.0, 2.0, 3.0])
@@ -64,7 +64,7 @@ class TestHessVecElementwiseUnivariate():
         square = cp.square(x)
         result_dict = square.hess_vec(vec)
         correct_matrix = np.diag([10.0, 8.0, 6.0])
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix))
 
     def test_constant_square(self):
         x = np.array([1.0, 2.0, 3.0])
@@ -81,7 +81,7 @@ class TestHessVecElementwiseUnivariate():
         power = cp.power(x, 3)
         result_dict = power.hess_vec(vec)
         correct_matrix = np.diag([30.0, 48.0, 54.0])
-        assert(np.allclose(result_dict[(x, x)], correct_matrix))
+        assert(np.allclose(result_dict[(x, x)].toarray(), correct_matrix))
 
     def test_constant_power(self):
         x = np.array([1.0, 2.0, 3.0])
@@ -89,5 +89,3 @@ class TestHessVecElementwiseUnivariate():
         power = cp.power(x, 3)
         result_dict = power.hess_vec(vec)
         assert(result_dict == {})
-
-