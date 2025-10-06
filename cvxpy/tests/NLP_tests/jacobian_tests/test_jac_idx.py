c80b84a8506cf4690fe1d2a14567ce105bbd3245
William Zijie Zhang <william@gridmatic.com>
2025-10-05 22:19:49 -0700

    getting matrix slice to work

--- cvxpy/tests/NLP_tests/jacobian_tests/test_jac_idx.py
+++ cvxpy/tests/NLP_tests/jacobian_tests/test_jac_idx.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import cvxpy as cp
 
@@ -24,12 +25,87 @@ class TestJacobianIndex():
         x = cp.Variable((n,), name='x')
         x.value = np.array([1.0, 2.0, 3.0])
         idxs = np.array([0, 2])
+        expr = cp.log(x)[idxs]
+        result_dict = expr.jacobian()
+        correct_jacobian = np.zeros((2, n))
+        correct_jacobian[0, 0] = 1/x.value[0]
+        correct_jacobian[1, 2] = 1/x.value[2]
+        computed_jacobian = np.zeros((2, n))
+        rows, cols, vals = result_dict[x]
+        computed_jacobian[rows, cols] = vals
+        assert(np.allclose(computed_jacobian, correct_jacobian))
 
     def test_jacobian_slice_idx(self):
-        pass
+        n = 4
+        x = cp.Variable((n,), name='x')
+        x.value = np.array([1.0, 2.0, 3.0, 4.0])
+        expr = cp.log(x)[1:3]
+        result_dict = expr.jacobian()
+        correct_jacobian = np.zeros((2, n))
+        correct_jacobian[0, 1] = 1/x.value[1]
+        correct_jacobian[1, 2] = 1/x.value[2]
+        computed_jacobian = np.zeros((2, n))
+        rows, cols, vals = result_dict[x]
+        computed_jacobian[rows, cols] = vals
+        assert(np.allclose(computed_jacobian, correct_jacobian))
 
+    pytest.mark.skip(reason="Boolean indexing not yet supported")
     def test_jacobian_special_idx(self):
-        pass
+        n = 4
+        x = cp.Variable((n,), name='x')
+        x.value = np.array([1.0, 2.0, 3.0, 4.0])
+        expr = cp.log(x)[[True, False, True, False]]
+        result_dict = expr.jacobian()
+        correct_jacobian = np.zeros((2, n))
+        correct_jacobian[0, 0] = 1/x.value[0]
+        correct_jacobian[1, 2] = 1/x.value[2]
+        computed_jacobian = np.zeros((2, n))
+        rows, cols, vals = result_dict[x]
+        computed_jacobian[rows, cols] = vals
+        assert(np.allclose(computed_jacobian, correct_jacobian))
+
+    ### Test matrix cases below ###
 
     def test_jacobian_matrix_slice(self):
-        pass
+        n = 2
+        x = cp.Variable((n, n), name='x')
+        x.value = np.array([[1.0, 2.0], [3.0, 4.0]])
+        expr = cp.log(x)[0, :]
+        result_dict = expr.jacobian()
+        correct_jacobian = np.zeros((2, n*n))
+        correct_jacobian[0, 0] = 1/x.value[0, 0]
+        correct_jacobian[1, 2] = 1/x.value[0, 1]
+        computed_jacobian = np.zeros((2, n*n))
+        rows, cols, vals = result_dict[x]
+        computed_jacobian[rows, cols] = vals
+        assert(np.allclose(computed_jacobian, correct_jacobian))
+
+    pytest.mark.skip(reason="Boolean indexing not yet supported")
+    def test_jacobian_matrix_special_idx(self):
+        n = 2
+        x = cp.Variable((n, n), name='x')
+        x.value = np.array([[1.0, 2.0], [3.0, 4.0]])
+        expr = cp.log(x)[[True, False], :]
+        result_dict = expr.jacobian()
+        correct_jacobian = np.zeros((2, n*n))
+        correct_jacobian[0, 0] = 1/x.value[0, 0]
+        correct_jacobian[1, 2] = 1/x.value[0, 1]
+        computed_jacobian = np.zeros((2, n*n))
+        rows, cols, vals = result_dict[x]
+        computed_jacobian[rows, cols] = vals
+        assert(np.allclose(computed_jacobian, correct_jacobian))
+
+    pytest.mark.skip(reason="List indexing not yet supported for matrices")
+    def test_jacobian_matrix_list_idx(self):
+        n = 2
+        x = cp.Variable((n, n), name='x')
+        x.value = np.array([[1.0, 2.0], [3.0, 4.0]])
+        expr = cp.log(x)[[0, 1], [1, 0]]
+        result_dict = expr.jacobian()
+        correct_jacobian = np.zeros((2, n*n))
+        correct_jacobian[0, 1] = 1/x.value[0, 1]
+        correct_jacobian[1, 2] = 1/x.value[1, 0]
+        computed_jacobian = np.zeros((2, n*n))
+        rows, cols, vals = result_dict[x]
+        computed_jacobian[rows, cols] = vals
+        assert(np.allclose(computed_jacobian, correct_jacobian))
