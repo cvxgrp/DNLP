e5d5a3dd21007c33cac7e66ccc4a52f3945797ae
William Zijie Zhang <william@gridmatic.com>
2025-10-22 07:16:49 -0700

    removing test for jacobian of none index

--- cvxpy/tests/NLP_tests/jacobian_tests/test_jac_idx.py
+++ cvxpy/tests/NLP_tests/jacobian_tests/test_jac_idx.py
@@ -109,20 +109,3 @@ class TestJacobianIndex():
         rows, cols, vals = result_dict[x]
         computed_jacobian[rows, cols] = vals
         assert(np.allclose(computed_jacobian, correct_jacobian))
-
-    @pytest.mark.xfail(reason="None indexing not yet supported for matrices")
-    def test_jacobian_none_idx(self):
-        n = 2
-        x = cp.Variable((n,), name='x')
-        x.value = np.array([1.0, 2.0])
-        expr1 = cp.log(x)[:, None]
-        expr2 = cp.log(x)[None, :]
-        expr = expr1 + expr2
-        result_dict = expr.jacobian()
-        correct_jacobian = np.zeros((n*n, n*n))
-        correct_jacobian[0, 0] = 1/x.value[0]
-        correct_jacobian[1, 1] = 1/x.value[1]
-        computed_jacobian = np.zeros((n*n, n*n))
-        rows, cols, vals = result_dict[x]
-        computed_jacobian[rows, cols] = vals
-        assert(np.allclose(computed_jacobian, correct_jacobian))
