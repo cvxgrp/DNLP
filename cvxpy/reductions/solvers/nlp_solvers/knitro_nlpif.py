9ef7361707e4afc879c3ad2910a789f9c1f06cf3
William Zijie Zhang <william@gridmatic.com>
2025-11-19 23:37:34 -0500

    adds initial changes for new interfaces

---
+++ cvxpy/reductions/solvers/nlp_solvers/knitro_nlpif.py
@@ -0,0 +1,168 @@
+"""
+Copyright 2025, the CVXPY developers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import numpy as np
+
+import cvxpy.settings as s
+from cvxpy.reductions.solution import Solution, failure_solution
+from cvxpy.reductions.solvers.nlp_solvers.nlp_solver import NLPsolver
+from cvxpy.utilities.citations import CITATION_DICT
+
+
+class KNITRO(NLPsolver):
+    """
+    NLP interface for the KNITRO solver
+    """
+
+    BOUNDED_VARIABLES = True
+    # Keys:
+    CONTEXT_KEY = "context"
+    X_INIT_KEY = "x_init"
+    Y_INIT_KEY = "y_init"
+
+    # Keyword arguments for the CVXPY interface.
+    INTERFACE_ARGS = [X_INIT_KEY, Y_INIT_KEY]
+
+    # Map of Knitro status to CVXPY status.
+    # This is based on the Knitro documentation:
+    # https://www.artelys.com/app/docs/knitro/3_referenceManual/returnCodes.html
+    STATUS_MAP = {
+        0: s.OPTIMAL,
+        -100: s.OPTIMAL_INACCURATE,
+        -101: s.USER_LIMIT,
+        -102: s.USER_LIMIT,
+        -103: s.USER_LIMIT,
+        -200: s.INFEASIBLE,
+        -201: s.INFEASIBLE,
+        -202: s.INFEASIBLE,
+        -203: s.INFEASIBLE,
+        -204: s.INFEASIBLE,
+        -205: s.INFEASIBLE,
+        -300: s.UNBOUNDED,
+        -301: s.UNBOUNDED,
+        -400: s.USER_LIMIT,
+        -401: s.USER_LIMIT,
+        -402: s.USER_LIMIT,
+        -403: s.USER_LIMIT,
+        -404: s.USER_LIMIT,
+        -405: s.USER_LIMIT,
+        -406: s.USER_LIMIT,
+        -410: s.USER_LIMIT,
+        -411: s.USER_LIMIT,
+        -412: s.USER_LIMIT,
+        -413: s.USER_LIMIT,
+        -415: s.USER_LIMIT,
+        -416: s.USER_LIMIT,
+        -500: s.SOLVER_ERROR,
+        -501: s.SOLVER_ERROR,
+        -502: s.SOLVER_ERROR,
+        -503: s.SOLVER_ERROR,
+        -504: s.SOLVER_ERROR,
+        -505: s.SOLVER_ERROR,
+        -506: s.SOLVER_ERROR,
+        -507: s.SOLVER_ERROR,
+        -508: s.SOLVER_ERROR,
+        -509: s.SOLVER_ERROR,
+        -510: s.SOLVER_ERROR,
+        -511: s.SOLVER_ERROR,
+        -512: s.SOLVER_ERROR,
+        -513: s.SOLVER_ERROR,
+        -514: s.SOLVER_ERROR,
+        -515: s.SOLVER_ERROR,
+        -516: s.SOLVER_ERROR,
+        -517: s.SOLVER_ERROR,
+        -518: s.SOLVER_ERROR,
+        -519: s.SOLVER_ERROR,
+        -520: s.SOLVER_ERROR,
+        -521: s.SOLVER_ERROR,
+        -522: s.SOLVER_ERROR,
+        -523: s.SOLVER_ERROR,
+        -524: s.SOLVER_ERROR,
+        -525: s.SOLVER_ERROR,
+        -526: s.SOLVER_ERROR,
+        -527: s.SOLVER_ERROR,
+        -528: s.SOLVER_ERROR,
+        -529: s.SOLVER_ERROR,
+        -530: s.SOLVER_ERROR,
+        -531: s.SOLVER_ERROR,
+        -532: s.SOLVER_ERROR,
+        -600: s.SOLVER_ERROR,
+    }
+
+    def name(self):
+        """
+        The name of solver.
+        """
+        return 'KNITRO'
+
+    def import_solver(self):
+        """
+        Imports the solver.
+        """
+        import knitro  # noqa F401
+
+    def invert(self, solution, inverse_data):
+        """
+        Returns the solution to the original problem given the inverse_data.
+        """
+        attr = {}
+        status = self.STATUS_MAP[solution['status']]
+        if status in s.SOLUTION_PRESENT:
+            primal_val = solution['obj_val']
+            opt_val = primal_val + inverse_data.offset
+            primal_vars = {}
+            x_opt = solution['x']
+            for id, offset in inverse_data.var_offsets.items():
+                shape = inverse_data.var_shapes[id]
+                size = np.prod(shape, dtype=int)
+                primal_vars[id] = np.reshape(x_opt[offset:offset+size], shape, order='F')
+            return Solution(status, opt_val, primal_vars, {}, attr)
+        else:
+            return failure_solution(status, attr)
+
+    def solve_via_data(self, data, warm_start: bool, verbose: bool, solver_opts, solver_cache=None):
+        """
+        Returns the result of the call to the solver.
+
+        Parameters
+        ----------
+        data : dict
+            Data used by the solver.
+        warm_start : bool
+            Not used.
+        verbose : bool
+            Should the solver print output?
+        solver_opts : dict
+            Additional arguments for the solver.
+        solver_cache: None
+            None
+
+        Returns
+        -------
+        tuple
+            (status, optimal value, primal, equality dual, inequality dual)
+        """
+        raise NotImplementedError("KNITRO NLP interface is not yet implemented.")
+
+    def cite(self, data):
+        """Returns bibtex citation for the solver.
+
+        Parameters
+        ----------
+        data : dict
+            Data generated via an apply call.
+        """
+        return CITATION_DICT["KNITRO"]
