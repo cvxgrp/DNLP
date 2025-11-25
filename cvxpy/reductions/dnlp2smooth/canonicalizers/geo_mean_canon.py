7e8d889a5a19270244e3de49ad28aba0c7001644
William Zijie Zhang <william@gridmatic.com>
2025-11-25 13:56:17 -0500

    adds cleanup and geomean first implementation

---
+++ cvxpy/reductions/dnlp2smooth/canonicalizers/geo_mean_canon.py
@@ -0,0 +1,37 @@
+"""
+Copyright 2025 CVXPY developers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import numpy as np
+
+from cvxpy.atoms.affine.sum import sum
+from cvxpy.atoms.elementwise.log import log
+from cvxpy.expressions.variable import Variable
+
+
+def geo_mean_canon(expr, args):
+    """
+    Canonicalization for the geometric mean function.
+    We reformulate geo_mean(x) as exp(1/n * sum(log(x)))
+    to form a diagonal hessian instead of a dense one.
+    """
+    t = Variable(args[0].shape, nonneg=True)
+    
+    if args[0].value is not None:
+        t.value = expr.numeric(args[0].value)
+    else:
+        t.value = np.ones(expr.shape)
+
+    return t, [log(t) == 1/expr.size * sum(log(args[0]))]
--- cvxpy/reductions/dnlp2smooth/canonicalizers/log_canon.py
+++ cvxpy/reductions/dnlp2smooth/canonicalizers/log_canon.py
@@ -16,11 +16,8 @@ limitations under the License.
 
 import numpy as np
 
-from cvxpy.expressions.constants import Constant
 from cvxpy.expressions.variable import Variable
 
-# DCED: Without this lower bound the stress test for ML Gaussian non-zero mean fails.
-# Perhaps this should be a parameter exposed to the user?
 LOWER_BOUND = 1e-5
 
 def log_canon(expr, args):
@@ -31,23 +28,4 @@ def log_canon(expr, args):
     else:
         t.value = expr.point_in_domain()
 
-    # DCED: introducing an out variable for log works MUCH worse for the 
-    # Gaussian ML problem 
-    #v = Variable(args[0].size)
-    #v.value = np.ones(expr.shape)
-    #v.value = np.log(t.value)
-    #return v, [v == expr.copy([t]), t == args[0]]
-
     return expr.copy([t]), [t == args[0]]
-
-# TODO (DCED): On some problems this canonicalization seems to work better.
-#              We should investigate this further when we have more benchmarks
-#              involving log.
-#def log_canon(expr, args):
-#    t = Variable(args[0].size)
-#    if args[0].value is not None and np.all(args[0].value > 0):
-#        t.value = np.log(args[0].value)
-#    else:
-#        t.value = expr.point_in_domain()
-
-#    return t, [exp(t) == args[0]]
