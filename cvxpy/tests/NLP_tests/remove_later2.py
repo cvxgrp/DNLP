263de0f4bd2ea4f1a5e9dab11e66e43825cbe025
William Zijie Zhang <william@gridmatic.com>
2025-11-28 12:10:38 -0500

    remove remove laters

--- cvxpy/tests/NLP_tests/remove_later2.py
+++
@@ -1,8 +0,0 @@
-import cvxpy as cp
-
-# minimize square root of x
-x = cp.Variable(bounds=[1e-8, None])
-obj = cp.Minimize(cp.sqrt(x))
-constraints = []
-problem = cp.Problem(obj, constraints)
-problem.solve(solver=cp.KNITRO, nlp=True, honorbnds=1, verbose=True, outlev=3, algorithm=6)
